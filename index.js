645302cc571ced735e3b070bcf8db9697118eb2d
bierbaszdev <bierbasz.it@gmail.com>
2022-04-25 21:57:17 +0200

    Added New Lighmode Background

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const toggle = document.querySelector('.toggle')
+const hero = document.querySelector('.hero')
 
 toggle.addEventListener('click', (e) => {
     const html = document.querySelector('html')
 
-    if(html.classList.contains('dark')) {
-        html.classList.remove('dark')
+    if(html.classList.contains('light')) {
+        html.classList.remove('light')
         e.target.innerHTML = 'Light mode'
+        hero.setAttribute('style', 'background-image: url(../assets/Background-img.jpg);')
     } else {
-        html.classList.add('dark')
+        html.classList.add('light')
         e.target.innerHTML = 'Dark mode'
+        hero.setAttribute('style', 'background-image: url(../assets/Background-img-light.jpg);')
     }
 })
