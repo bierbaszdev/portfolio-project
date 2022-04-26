599ed13b510c0cf9670435bf2e6704d5113de405
bierbaszdev <92809535+bierbaszdev@users.noreply.github.com>
2022-04-26 08:52:01 +0200

    Changed BackgroundIMG PATHS

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ toggle.addEventListener('click', (e) => {
     if(html.classList.contains('light')) {
         html.classList.remove('light')
         e.target.innerHTML = 'Light mode'
-        hero.setAttribute('style', 'background-image: url(../assets/Background-img.jpg);')
+        hero.setAttribute('style', 'background-image: url(./assets/Background-img.jpg);')
     } else {
         html.classList.add('light')
         e.target.innerHTML = 'Dark mode'
-        hero.setAttribute('style', 'background-image: url(../assets/Background-img-light.jpg);')
+        hero.setAttribute('style', 'background-image: url(./assets/Background-img-light.jpg);')
     }
 })
